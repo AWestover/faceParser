e0ca7023d636e627699e2e9f23e9176254014d46
awestover <alek.westover@gmail.com>
2018-08-10 23:10:48 -0400

    downloads

--- static/sketch.js
+++ static/sketch.js
@@ -1,5 +1,6 @@
 
 let dim = Math.min(window.innerWidth*0.5, window.innerHeight*0.5);
+let img_ct = 0;
 
 Webcam.set({
   width: dim,
@@ -17,13 +18,20 @@ function takeScreenshot()
     $('body').prepend('<div class="loader"></div>');
     $.post("/upload", {"data_uri": data_uri, "num_colors": $('#num_colors').val()}, function(data)
       {
+          img_ct += 1;
           let loaders = $('.loader');
           for (let i = 0; i < loaders.length; i++)
           {
             loaders[0].remove();
           }
           $.notify("Your face has arrived", "success");
-          $("#output_pics").append("<img width='90%' src='" + data.path + "'>");
+          let newImgHTML = "<div id='img_"+img_ct+"'>";
+          newImgHTML += "<img width='90%' src='" + data.path + "'>";
+          newImgHTML += "<button onclick='$(\"#img_"+img_ct+"\").remove();'>Delete image</button>"
+          newImgHTML += "<a href='"+data.path+"' download='weird-face.png'>";
+          newImgHTML += "<button class='btn'>Download image</button></a>"
+          newImgHTML += "</div>";    
+          $("#output_pics").append(newImgHTML);
           window.scrollTo(0, document.body.scrollHeight);
       });
   });
